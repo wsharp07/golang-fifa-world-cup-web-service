6b7d282e0c60da9f6ffca9807012745d244b6933
William Sharp <wsharp07@gmail.com>
2021-05-05 20:41:11 -0400

    module 3

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -38,6 +38,21 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+
+	isTokenValid := data.IsAccessTokenValid(accessToken)
+
+	if !isTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+		return
+	} else {
+		err := data.AddNewWinner(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+	}
 
 }
 
